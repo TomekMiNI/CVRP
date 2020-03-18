16fdde145526bf5e17c46810334afefa202e739c
TOmI mARKO <tomekmarkowicz3@wp.pl>
2020-03-18 17:48:04 +0100

    probably all structures and some, not finished, code...

--- CVRP/CVRP.cs
+++ CVRP/CVRP.cs
@@ -19,47 +19,136 @@ namespace CVRP
     //rank correction
     //evaporation decrease correction
 
-    //input graph, constant values
-    //demands and distances
+    /// <summary>
+    /// Current state 
+    /// </summary>
     private Graph State { get; set; }
-    
-    private int MAX_ITER { get; }
-
-    private int Amount { get; }
-
+    /// <summary>
+    /// Max number of iterations
+    /// </summary>
+    private int MaxIter { get; }
+    /// <summary>
+    /// Count of ants
+    /// </summary>
+    private int CountOfAnts { get; }
+    /// <summary>
+    /// Evaporation factor
+    /// </summary>
     private double EvaporationFactor { get; }
+    /// <summary>
+    /// Each ant has the same capacity
+    /// </summary>
+    private double Capacity { get; }
+
+    /// <summary>
+    /// Count of vertices
+    /// </summary>
+    private int CountOfVertices { get; }
+    /// <summary>
+    /// Weight of pheromone
+    /// </summary>
+    private double Alpha { get; }
+    /// <summary>
+    /// Weight of distance
+    /// </summary>
+    private double Beta { get; }
+
+    //modifications
     private Type AlgorithmType { get; }
     private int CountOfRankAnts { get; }
 
+    public CVRP(int countOfAnts, int maxIter, double evaporationFactor, int[,] distances, double alpha, double beta, double[,] pheromones = null)
+    {
+      CountOfAnts = countOfAnts;
+      MaxIter = maxIter;
+      EvaporationFactor = evaporationFactor;
+      CountOfVertices = CountOfAnts + 1;
+
+      Alpha = alpha;
+      Beta = beta;
+
+      State = new Graph(CountOfVertices, distances, pheromones);
+    }
+
     public Solution Run()
     {
-
+      var bestSolution = new Solution();
       //MAX_ITER iterations
-      for (int i = 0; i < MAX_ITER; i++)
+      for (int i = 0; i < MaxIter; i++)
       {
-        for (int ant = 0; ant < Amount; ant++)
+        var solutions = new Solution[CountOfAnts];
+        for (int ant = 0; ant < CountOfAnts; ant++)
         {
-          FindSolution(ant);
+           solutions[ant] = FindSolution(ant);
         }
-        CorrectPheromones(AlgorithmType);
+        var minLocalSolution = solutions.Min();
+
+        if (minLocalSolution.Value < bestSolution.Value)
+          bestSolution = minLocalSolution;
+
+        UpdatePheromones(solutions);
       }
-
-
-      return new Solution();
+      return bestSolution;
     }
 
     public Solution FindSolution(int ant)
     {
       Solution solution = new Solution();
 
+      bool[] alreadyVisitedArr = new bool[CountOfVertices];
+      //ant starts in [ant + 1]th vertice in GRAPH [0 is a depot]
+      alreadyVisitedArr[ant + 1] = true;
+      int alreadyVisited = 1;
 
+      int start = ant + 1;
+
+      while(alreadyVisited < CountOfAnts)
+      {
+        //1. phase: Choosing next vertice to visit
+        double currentMax = 0;
+        int currentWinner = -1;
+        //We avoid depot. We go there when we have no other option
+        for(int i = 1; i < CountOfVertices; i++)
+          if(!alreadyVisitedArr[i])
+          {
+            double candidate = Math.Pow(State[start, i].Pheromone, Alpha) * Math.Pow(State[start, i].Distance, Beta);
+            if(candidate > currentMax)
+            {
+              currentMax = candidate;
+              currentWinner = i;
+            }
+          }
+
+        //next vertice chosen
+        start = currentWinner;
+      }
 
       return solution;
     }
 
-    public void CorrectPheromones(Type type)
+    public void UpdatePheromones(Solution[] solutions)
     {
+      double constW = CountOfAnts; //??
+      double[,] pheromonesIncrease = new double[CountOfVertices, CountOfVertices];
 
+      //leaving pheromone
+      foreach (var sol in solutions)
+      {
+        foreach (var route in sol.Routes)
+        {
+          foreach (var edge in route.Edges)
+          {
+            double val = constW / sol.Value;
+            pheromonesIncrease[edge.V1, edge.V1] += val;
+            pheromonesIncrease[edge.V2, edge.V2] += val;
+          }
+        }
+      }
+
+      //evaporation and pheromone increase
+      for (int i = 0; i < CountOfVertices - 1; i++)
+        for (int j = i + 1; j < CountOfVertices; j++)
+          State[i, j].Pheromone = State[i, j].Pheromone * EvaporationFactor + pheromonesIncrease[i, j];
     }
   }
 }
--- CVRP/Structures.cs
+++ CVRP/Structures.cs
@@ -12,44 +12,105 @@ namespace CVRP
     Edge[,] edges;
 
     public int this[int i] => vertices[i];
-    public Edge this[int i, int j] => edges[Math.Min(i, j), Math.Max(i,j)]; //upper
+    public Edge this[int i, int j]
+    {
+      get {
+        return edges[Math.Min(i, j), Math.Max(i, j)]; //upper
+      }
+      set {
+        edges[i, j] = value;
+      }
+    }
+
+    /// <summary>
+    /// Graph with distances and pheromones
+    /// </summary>
+    /// <param name="size"></param>
+    /// <param name="distances"></param>
+    /// <param name="pheromones">If null every edge starts with pheromone equals 0</param>
+    public Graph(int size, int[,] distances, double[,] pheromones = null)
+    {
+      vertices = new int[size];
+      if (pheromones == null)
+        pheromones = new double[size, size];
+      edges = new Edge[size, size];
+      for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j < size; j++)
+          edges[i, j] = new Edge(i, j, distances[i, j], pheromones[i, j]);
+    }
   }
 
-  class Edge
+  public class Vertice
   {
-    int v1, v2;
-    int distance;
-    double pheromone;
+    public int Id { get; }
+    public int Demand { get; }
 
-    public Edge(int v1, int v2, int distance)
+    public Vertice(int id, int demand)
     {
-        this.v1 = v1;
-        this.v2 = v2;
-        this.distance = distance;
+      Id = id;
+      Demand = demand;
+    }
+  }
+
+  public class Edge
+  {
+    /// <summary>
+    /// (v1, v2) = (v2, v1) - because of undirected graph
+    /// </summary>
+    public int V1 { get; set; }
+    public int V2 { get; set; }
+    /// <summary>
+    /// Euclidean distance between vertices with indexes v1 and v2
+    /// </summary>
+    public int Distance { get; set; }
+    /// <summary>
+    /// current pheromone value
+    /// </summary>
+    public double Pheromone { get; set; }
+
+    public Edge(int v1, int v2, int distance, double pheromone)
+    {
+      V1 = v1;
+      V2 = v2;
+      Distance = distance;
+      Pheromone = pheromone;
     }
 
     public override string ToString()
     {
-      return base.ToString();
+      return string.Format("<{0}, {1}>", Math.Min(v1, v2), Math.Max(v1, v2));
     }
   }
 
   class Route
   {
-    List<Edge> route;
+    public List<Edge> Edges;
 
     public override string ToString()
     {
-      return base.ToString();
+      StringBuilder sb = new StringBuilder();
+      sb.Append('[');
+      Edges.ForEach(e => sb.Append(e.ToString()));
+      sb.Append(']');
+      return sb.ToString();
     }
   }
 
-  class Solution
+  class Solution : IComparable<Solution>
   {
-    List<Route> path;
+    public List<Route> Routes;
+    public int Value;
+
+    public int CompareTo(Solution other)
+    {
+      return Value < other.Value ? 1 : -1;
+    }
+
     public override string ToString()
     {
-      return base.ToString();
+      StringBuilder sb = new StringBuilder();
+      Routes.ForEach(r => sb.AppendLine(r.ToString()));
+      return sb.ToString();
     }
   }
   
